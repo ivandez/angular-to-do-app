d3e10fa1fc7a71e3fa07429d64453866ea03eb49
Ivan Mendez <ivanmr932@gmail.com>
2020-10-04 10:56:58 -0400

    print taks

--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -1,5 +1,3 @@
 import { Task } from './task';
 
-export const TASK: Task[] = [
-  { id: 1, description: 'esto es una prueba', done: false },
-];
+export const TASK: Task[] = [];
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { TASK } from '../mock-task';
 
 @Component({
   selector: 'app-task',
@@ -10,6 +11,7 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 export class TaskComponent implements OnInit {
   faTrash = faTrash;
   faCheck = faCheck;
+  tasks = TASK;
   constructor() {}
 
   ngOnInit(): void {}
