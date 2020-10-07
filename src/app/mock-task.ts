de951f0acd161532abfc15b140c02a9f0577bb30
Ivan Mendez <ivanmr932@gmail.com>
2020-10-07 14:36:44 -0400

    end desktop styles

--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -1,8 +1,3 @@
 import { Task } from './task';
 
-export const TASK: Task[] = [
-  { id: 0, description: '0 text', done: false },
-  { id: 1, description: '1 text', done: false },
-  { id: 2, description: '2 text', done: true },
-  { id: 3, description: '3 text', done: false },
-];
+export const TASK: Task[] = [];
