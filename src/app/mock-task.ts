e6153dd0b180329cf03b629dbe274ee31f034133
Ivan Mendez <ivanmr932@gmail.com>
2020-10-07 13:28:55 -0400

    refactored deleteTask code
    Because it was to hard to understand

--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -1,8 +1,8 @@
 import { Task } from './task';
 
 export const TASK: Task[] = [
-  { id: 0, description: '0', done: false },
-  { id: 1, description: '1', done: false },
-  { id: 2, description: '2', done: true },
-  { id: 3, description: '3', done: false },
+  { id: 0, description: '0 text', done: false },
+  { id: 1, description: '1 text', done: false },
+  { id: 2, description: '2 text', done: true },
+  { id: 3, description: '3 text', done: false },
 ];
